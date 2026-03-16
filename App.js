2b0b559832448b49ed1fc133e96faeb62117d416
SWEETBE <161703885+SWEETBE@users.noreply.github.com>
2026-03-16 23:26:59 +0300

    Create App.js as main entry point for React Native application

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+// Register main app
+AppRegistry.registerComponent('SWEETGAMES_APP', () => App);
