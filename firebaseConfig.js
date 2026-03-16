51661057021db23b5b164383b60ad89d9de68dc3
SWEETBE <161703885+SWEETBE@users.noreply.github.com>
2026-03-16 23:25:41 +0300

    Create firebaseConfig.js file with Firebase configuration.

---
+++ firebaseConfig.js
@@ -0,0 +1,28 @@
+// firebaseConfig.js
+
+// Import the Firebase libraries
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+export { app, auth, database, storage };
